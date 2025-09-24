9e824f31be5d00dfd49de7e705809b6e8b495549
JaviFebo <javi.bolanos.c@gmail.com>
2025-09-23 23:42:52 -0600

    Función factorial arreglada

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,17 +1,21 @@
-#include <studio.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int factorial (int n) {
-	int i = i;
+	int i = 1;
+	if ( n == 1 ) {
+		return i;
+	}
 	while (n > 1) {
 		i = i * n;
-		int n = n - 1;
+		n = n - 1;
 	}
 	return i;
 }
 
 int main (int argc, char *argv[]) {
-	int fac4 = factorial(4);
-	int fac5 = factorial(5);
+	int fac4 = factorial (4);
+	int fac5 = factorial (5);
 	printf("4! =  %d, 5! = %d\n", fac4, fac5);
 	return 0;
 }
