3c9a89877b408f295bf4fbd4b9f4925522261ef6
JaviFebo <javi.bolanos.c@gmail.com>
2025-09-24 20:07:48 -0600

    Cambios revertidos

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define SIZE 3
 
@@ -38,34 +40,88 @@ int sumar_diagSec (int matriz[SIZE][SIZE]) {
 
 	for (int i = 0, j = SIZE-1; i < SIZE; i++, j--) {
 		suma = suma + matriz[i][j];
-		printf ("i = %d\n", i);
-		printf ("j = %d\n", j);
-		printf ("m[i][j] = %d\n", matriz[i][j]);
-		printf ("Suma = %d\n", suma);
 	}
 
 	return suma;
 }
 
+int es_cuadradoMagico (int matriz[SIZE][SIZE]) {
+	int suma_magica = sumar_linea (matriz, 0);
+	
+	int suma_diag_prim = sumar_diagPrim (matriz);
+	int suma_diag_sec = sumar_diagSec (matriz);
+
+	if (suma_diag_prim != suma_magica || suma_diag_sec != suma_magica) {
+		return 0;
+	}
+
+
+	for (int i = 0; i < SIZE; i++) {
+		int suma_linea_i = sumar_linea (matriz, i);
+		if (suma_linea_i != suma_magica) {
+			return 0;
+		}
+	}
+
+	for (int j = 0; j < SIZE; j++) {
+		int suma_col_j = sumar_columna (matriz, j);
+		if (suma_col_j != suma_magica) {
+			return 0;
+		}
+	}
+
+	return 1;
+}
+
+int imprimir_matriz (int matriz [SIZE][SIZE]) {
+	for (int i = 0; i < SIZE; ++i) {
+		for (int j = 0; j < SIZE; ++j) {
+			printf (" %d ", matriz[i][j]);
+		}
+		printf ("\n");
+	}
+	return 0;
+}
+
+void poblar_matrizRand (int matriz [SIZE][SIZE]) {
+	srand (time(NULL));
+	int max = 9;
+	int min = 0;
+
+	for (int i = 0; i < SIZE; ++i) {
+		for (int j = 0; j < SIZE; ++j) {
+			matriz [i][j] = rand() % (max + 1 - min) + min;
+		}
+	}
+}
 
 int main () {
-	int matriz [SIZE][SIZE] = {
+	int matriz_const [SIZE][SIZE] = {
 		{2, 7, 6},
 		{9, 5, 1},
 		{4, 3, 8}
 	};
+
+	printf ("La matriz constante utilizada fue:\n");
+	imprimir_matriz (matriz_const);
 	
-
-	int suma_linea = sumar_linea (matriz, 0);
-	int suma_columna = sumar_columna (matriz, 0);
-	int suma_diagPrim = sumar_diagPrim (matriz);
-	int suma_diagSec = sumar_diagSec (matriz);
-
-	printf ("La suma de la línea 1 es: %d\n", suma_linea);
-	printf ("La suma de la columna 1 es: %d\n", suma_columna);
-	printf ("La suma de la diagonal primaria es: %d\n", suma_diagPrim);
-	printf ("La suma de la diagonal secundaria es: %d\n", suma_diagSec);
-
+	if (es_cuadradoMagico(matriz_const)) {
+		printf ("La matriz es un cuadrado mágico\n");
+	} else {
+		printf ("La matriz no es un cuadrado mágico\n");
+	}
+	
+	int matriz_rand [SIZE][SIZE];
+	poblar_matrizRand(matriz_rand);
+	
+	printf ("\nLa matriz aleatoria utilizada fue:\n");
+	imprimir_matriz (matriz_rand);
+	
+	if (es_cuadradoMagico(matriz_rand)) {
+		printf ("La matriz es un cuadrado mágico\n");
+	} else {
+		printf ("La matriz no es un cuadrado mágico\n");
+	}
+	
 	return 0;
 }
-
