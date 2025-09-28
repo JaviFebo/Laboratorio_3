4b23c2cf2fc199ab0d67f0ec41867c09184bf366
JaviFebo <javi.bolanos.c@gmail.com>
2025-09-27 18:51:37 -0600

    Añadida documentación

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+//Función que calcula n!
 int factorial (int n) {
 	int i = 1;
-	if ( n == 1 ) {
-		return i;
+	if ( n == 1 || n == 0 ) {
+		return i; //Si n es 1 o 0 devuelve 1
 	}
 	while (n > 1) {
 		i = i * n;
@@ -15,7 +15,7 @@ int factorial (int n) {
 }
 
 int main (int argc, char *argv[]) {
-	int n = atoi(argv[1]);
+	int n = atoi(argv[1]); //Guarda el primer argumento que se le pasa al programa como int
 	printf("%d! =  %d\n", n, factorial(n));
 	return 0;
 }
--- ejercicio2.c
+++ ejercicio2.c
@@ -4,68 +4,72 @@
 
 #define SIZE 3
 
+//Función que suma todos los elementos en una línea de una matriz
 int sumar_linea (int matriz[SIZE][SIZE], int i) {
 	int suma = 0;
 
-	for (int j = 0; j < SIZE; j++) {
+	for (int j = 0; j < SIZE; j++) { //Se itera por cada columna en una línea de la matriz
 		suma = suma + matriz[i][j];
 	}
 
 	return suma;
 }
 
-
+//Función que suma todos los elementos en una columna de una matriz
 int sumar_columna (int matriz[SIZE][SIZE], int j) {
 	int suma = 0;
 
-	for (int i = 0; i < SIZE; i++) {
+	for (int i = 0; i < SIZE; i++) { //Se itera por cada línea en una columna de la matriz
 		suma = suma + matriz[i][j];
 	}
 
 	return suma;
 }
 
+//Función que suma todos los elementos de la diagonal principal en una matriz
 int sumar_diagPrim (int matriz[SIZE][SIZE]) {
 	int suma = 0;
 
-	for (int i = 0, j = 0; i < SIZE; i++, j++) {
+	for (int i = 0, j = 0; i < SIZE; i++, j++) { //Se recorre la diagonal principal de la matriz
 		suma = suma + matriz[i][j];
 	}
 
 	return suma;
 }
 
+//Función que suma todos los elementos de la diagonal secundaria en una matriz
 int sumar_diagSec (int matriz[SIZE][SIZE]) {
 	int suma = 0;
 
-	for (int i = 0, j = SIZE-1; i < SIZE; i++, j--) {
+	for (int i = 0, j = SIZE-1; i < SIZE; i++, j--) { //Se recorre la diagonal secundaria de la matriz
 		suma = suma + matriz[i][j];
 	}
 
 	return suma;
 }
 
+//Función que verifica si una matriz es un cuadrado mágico
 int es_cuadradoMagico (int matriz[SIZE][SIZE]) {
-	int suma_magica = sumar_linea (matriz, 0);
+	int suma_magica = sumar_linea (matriz, 0); //Se obtiene el valor mágico de sumar los elementos de la primera línea
 	
 	int suma_diag_prim = sumar_diagPrim (matriz);
 	int suma_diag_sec = sumar_diagSec (matriz);
 
-	if (suma_diag_prim != suma_magica || suma_diag_sec != suma_magica) {
+	if (suma_diag_prim != suma_magica || suma_diag_sec != suma_magica) { //Si la suma de las diagonales no es igual al valor mágico retorna falso
 		return 0;
 	}
 
 
-	for (int i = 0; i < SIZE; i++) {
+	for (int i = 0; i < SIZE; i++) { //Se itera por cada línea de la matriz
 		int suma_linea_i = sumar_linea (matriz, i);
-		if (suma_linea_i != suma_magica) {
+		if (suma_linea_i != suma_magica) { //Si la suma de una línea no es igual al valor mágico retorna falso
 			return 0;
 		}
 	}
 
-	for (int j = 0; j < SIZE; j++) {
+	for (int j = 0; j < SIZE; j++) { //Se itera por cada columna de la matriz
 		int suma_col_j = sumar_columna (matriz, j);
-		if (suma_col_j != suma_magica) {
+		if (suma_col_j != suma_magica) { //Si la suma de una columna no es igual al valor mágico retorna falso
 			return 0;
 		}
 	}
@@ -73,9 +77,10 @@ int es_cuadradoMagico (int matriz[SIZE][SIZE]) {
 	return 1;
 }
 
+//Función que imprime una matriz línea por línea
 int imprimir_matriz (int matriz [SIZE][SIZE]) {
-	for (int i = 0; i < SIZE; ++i) {
-		for (int j = 0; j < SIZE; ++j) {
+	for (int i = 0; i < SIZE; ++i) { //Se itera cada línea de la matriz
+		for (int j = 0; j < SIZE; ++j) { //Se itera cada columna de la matriz
 			printf (" %d ", matriz[i][j]);
 		}
 		printf ("\n");
@@ -83,10 +88,11 @@ int imprimir_matriz (int matriz [SIZE][SIZE]) {
 	return 0;
 }
 
+//Función que genera una matriz cuadrada con valores aleatorios
 void poblar_matrizRand (int matriz [SIZE][SIZE]) {
 	srand (time(NULL));
-	int max = 9;
-	int min = 0;
+	int max = 9; //El valor máximo para poblar la matriz es 9
+	int min = 0; //El valor mínimo para poblar la matriz es 0
 
 	for (int i = 0; i < SIZE; ++i) {
 		for (int j = 0; j < SIZE; ++j) {
@@ -96,7 +102,7 @@ void poblar_matrizRand (int matriz [SIZE][SIZE]) {
 }
 
 int main () {
-	int matriz_const [SIZE][SIZE] = {
+	int matriz_const [SIZE][SIZE] = { //Se inicializa una matriz constante
 		{2, 7, 6},
 		{9, 5, 1},
 		{4, 3, 8}
@@ -111,8 +117,8 @@ int main () {
 		printf ("La matriz no es un cuadrado mágico\n");
 	}
 	
-	int matriz_rand [SIZE][SIZE];
-	poblar_matrizRand(matriz_rand);
+	int matriz_rand [SIZE][SIZE]; //Se inicializa la matriz que será poblada de forma aleatoria
+	poblar_matrizRand(matriz_rand); //Se pobla la matriz de forma aleatoria
 	
 	printf ("\nLa matriz aleatoria utilizada fue:\n");
 	imprimir_matriz (matriz_rand);
